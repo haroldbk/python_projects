59dde3a093b7e3d7c057155fba098d4869e350bd
Harold Kless <haroldbk@msn.com>
2025-09-16 12:20:40 -0500

    Update udp_chat.py

--- chat/udp_chat.py
+++ chat/udp_chat.py
@@ -5,6 +5,54 @@ from PyQt5 import QtCore as qtc
 from PyQt5 import QtGui as qtg
 from PyQt5 import QtNetwork as qtn
 
+
+class UdpChatInterface(qtc.QObject):
+    port=7777
+    delimer='||'
+    received = qtc.pyqtSignal(str,str)
+    error=qtc.pyqtSignal(str)
+    def __init__(self,username):
+        super().__init__()
+        self.username=username
+        self.socket = qtn.QUdpSocket()
+        self.socket.bind(qtn.QHostAddress.Any, self.port)
+        self.socket.readyRead.connect(self.process_datagrams)
+        self.socket.error.connect(self.on_error)
+
+    def process_datagrams(self):
+        while self.socket.hasPendingDatagrams():
+            datagram=self.socket.receiveDatagram()
+            # to convert QByteArray to a string,
+            # cast it to bytes then decode
+            raw_message = bytes(datagram.data()).decode('utf-8')
+
+            if self.delimer not in raw_message:
+                # invalid message ignore
+                continue
+            username,message = raw_message.split(self.delimer,1)
+            self.received.emit(username,message)
+
+    def on_error(self, socket_error):
+        # Magic to get the enum name
+        error_index = (qtn.QAbstractSocket.staticMetaObject
+                       .indexOfEnumerator('SocketError'))
+        error = (qtn.QAbstractSocket
+                 .staticMetaObject
+                 .enumerator(error_index)
+                 .valueToKey(socket_error))
+        message = f'There was a network error: {error}'
+        self.error.emit(message)
+    def send_message (self, message):
+        # prepare and send a message #
+        msg_bytes= (
+            f'{self.username} {self.delimer}{message}'
+        ).encode('utf-8')
+        self.socket.writeDatagram(
+            qtc.QByteArray(msg_bytes),
+            qtn.QHostAddress.Broadcast,
+            self.port
+        )
+
 class ChatWindow(qtw.QWidget):
     submitted = qtc.pyqtSignal(str)
     def __init__(self):
@@ -47,6 +95,7 @@ if __name__=='__main__':
     app =qtw.QApplication(sys.argv)
     w = MainWindow()
     sys.exit(app.exec_())
+
 
 
 
